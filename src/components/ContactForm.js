15ea28505c4019fbe6062c64953228ce8d8d212f
Sly <slymasterfunk@gmail.com>
2021-10-29 11:09:37 -0500

    working form issue2

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -32,7 +32,8 @@ class ContactForm extends React.Component {
     render() {
         const { name, email, message } = this.state;
         return (
-            <form netlify name='contact' onSubmit={this.handleSubmit}>
+            <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={this.handleSubmit}>
+                <input type="hidden" name="form-name" value="contact" />
                 <p>
                     <label>
                         Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
